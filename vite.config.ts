84d8fed573912897fc86e68406e5191b48cdbcba
abochman <abochman@moontiger.us>
2024-11-12 11:18:34 -0500

    Init commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/portfolio/', // Replace 'your-repo-name' with your actual repository name
+});
